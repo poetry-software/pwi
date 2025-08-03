0275aeee8bc1aafdb36d2ebfef1c6a47ec4bfd68
Alec DuBois <after.alec@gmail.com>
2025-08-03 12:29:26 -0700

    Update home route to just render dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return redirect()->route('dashboard');
-})->name('home');
-
-Route::get('dashboard', function () {
     return Inertia::render('dashboard');
-})->name('dashboard');
+})->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {});
 
