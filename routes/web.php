fe0f22ca00901bfaffb4a5736f27b558e730a02a
Alec DuBois <after.alec@gmail.com>
2025-08-03 12:37:23 -0700

    Add Dashboard Route Back

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return Inertia::render('dashboard');
 })->name('home');
 
+Route::get('dashboard', function () {
+    return Inertia::render('dashboard');
+})->name('dashboard');
+
 Route::middleware(['auth', 'verified'])->group(function () {});
 
 require __DIR__.'/settings.php';
